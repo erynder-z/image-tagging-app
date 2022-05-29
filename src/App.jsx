388f3aab80e320bcc17dcd59222f4d184f86cc50
erynder-z <stfn.bgr@gmail.com>
2022-05-29 18:37:20 +0200

    Move validation logic to app component

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import './App.css';
 import React, { useState } from 'react';
 import { Routes, Route } from 'react-router-dom';
 import { initializeApp } from 'firebase/app';
-import { getFirestore } from 'firebase/firestore';
+import { getFirestore, doc, getDoc } from 'firebase/firestore';
 import Nav from './components/Nav/Nav';
 import StartScreen from './components/StartScreen/StartScreen';
 import Welcome from './components/Welcome/Welcome';
@@ -19,13 +19,75 @@ function App() {
   };
 
   const app = initializeApp(firebaseConfig);
-  // eslint-disable-next-line no-unused-vars
   const database = getFirestore(app);
   const [user, setUser] = useState({
     name: 'Jack',
     gamStartTime: 1,
     id: 'someID'
   });
+  const [relativeX, setRelativeX] = useState();
+  const [relativeY, setRelativeY] = useState();
+  const [targets, setTargets] = useState([
+    {
+      name: 'target1',
+      found: false
+    },
+    {
+      name: 'target2',
+      found: false
+    },
+    {
+      name: 'target3',
+      found: false
+    }
+  ]);
+
+  const markFound = (target) => {
+    setTargets(
+      [...targets].map((object) => {
+        if (object.name === target.name) {
+          return {
+            ...object,
+            found: true
+          };
+        }
+        return object;
+      })
+    );
+  };
+
+  const checkTarget = async (proposedTarget) => {
+    const getData = async () => {
+      const docRef = doc(database, 'targets', proposedTarget);
+      const docSnap = await getDoc(docRef);
+
+      if (docSnap.exists()) {
+        return docSnap.data().coords;
+      }
+      console.log('No such document!');
+      return null;
+    };
+
+    const target = targets.find((item) => item.name === proposedTarget);
+    const targetCoords = await getData();
+
+    const targetX1 = targetCoords[0];
+    const targetY1 = targetCoords[1];
+    const targetX2 = targetCoords[2];
+    const targetY2 = targetCoords[3];
+
+    if (
+      relativeX >= targetX1 &&
+      relativeX <= targetX2 &&
+      relativeY >= targetY1 &&
+      relativeY <= targetY2
+    ) {
+      console.log(`found ${target.name}`);
+      markFound(target);
+    } else {
+      console.log('false');
+    }
+  };
 
   const saveName = (formValue) => {
     setUser({
@@ -51,7 +113,17 @@ function App() {
             />
           }
         />
-        <Route path="/gameimage" element={<GameImage database={database} user={user} />} />
+        <Route
+          path="/gameimage"
+          element={
+            <GameImage
+              user={user}
+              setRelativeX={setRelativeX}
+              setRelativeY={setRelativeY}
+              checkTarget={checkTarget}
+            />
+          }
+        />
         {/*   <Route path="/something" element={<Something />} /> */}
       </Routes>
     </div>
--- src/components/GameImage/GameImage.jsx
+++ src/components/GameImage/GameImage.jsx
@@ -2,31 +2,15 @@
 /* eslint-disable jsx-a11y/no-static-element-interactions */
 import './GameImage.css';
 import React, { useEffect, useState, useRef } from 'react';
-import { doc, getDoc } from 'firebase/firestore';
 import PropTypes from 'prop-types';
 import image from '../../assets/wimmelbild.jpg';
 import Popupmenu from '../Popupmenu/Popupmenu';
 
-function GameImage({ database }) {
+function GameImage({ setRelativeX, setRelativeY, checkTarget }) {
   const highlighter = useRef();
-  const [targets, setTargets] = useState([
-    {
-      name: 'target1',
-      found: false
-    },
-    {
-      name: 'target2',
-      found: false
-    },
-    {
-      name: 'target3',
-      found: false
-    }
-  ]);
+
   const [clickX, setClickX] = useState();
   const [clickY, setClickY] = useState();
-  const [relativeX, setRelativeX] = useState();
-  const [relativeY, setRelativeY] = useState();
   const [showPopup, setShowPopup] = useState(false);
 
   const getClickCoordinates = (e) => {
@@ -38,53 +22,6 @@ function GameImage({ database }) {
 
   const handleClick = () => {
     setShowPopup(!showPopup);
-  };
-
-  const markFound = (target) => {
-    setTargets(
-      [...targets].map((object) => {
-        if (object.name === target.name) {
-          return {
-            ...object,
-            found: true
-          };
-        }
-        return object;
-      })
-    );
-  };
-
-  const checkTarget = async (proposedTarget) => {
-    const getData = async () => {
-      const docRef = doc(database, 'targets', proposedTarget);
-      const docSnap = await getDoc(docRef);
-
-      if (docSnap.exists()) {
-        return docSnap.data().coords;
-      }
-      console.log('No such document!');
-      return null;
-    };
-
-    const target = targets.find((item) => item.name === proposedTarget);
-    const targetCoords = await getData();
-
-    const targetX1 = targetCoords[0];
-    const targetY1 = targetCoords[1];
-    const targetX2 = targetCoords[2];
-    const targetY2 = targetCoords[3];
-
-    if (
-      relativeX >= targetX1 &&
-      relativeX <= targetX2 &&
-      relativeY >= targetY1 &&
-      relativeY <= targetY2
-    ) {
-      console.log(`found ${target.name}`);
-      markFound(target);
-    } else {
-      console.log('false');
-    }
   };
 
   useEffect(() => {
@@ -121,6 +58,7 @@ function GameImage({ database }) {
 export default GameImage;
 
 GameImage.propTypes = {
-  // eslint-disable-next-line react/forbid-prop-types
-  database: PropTypes.object.isRequired
+  setRelativeX: PropTypes.func.isRequired,
+  setRelativeY: PropTypes.func.isRequired,
+  checkTarget: PropTypes.func.isRequired
 };
