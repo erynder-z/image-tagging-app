6d2495ca51448c9954f2400c98e783a2874ff4b4
erynder-z <stfn.bgr@gmail.com>
2022-05-29 14:56:02 +0200

    Create a list of proposed targets

--- src/components/Popupmenu/Popupmenu.jsx
+++ src/components/Popupmenu/Popupmenu.jsx
@@ -1,8 +1,13 @@
+/* eslint-disable jsx-a11y/no-noninteractive-element-interactions */
+/* eslint-disable jsx-a11y/click-events-have-key-events */
 import './Popupmenu.css';
 import PropTypes from 'prop-types';
 import React from 'react';
+import image1 from '../../assets/target1.png';
+import image2 from '../../assets/target2.png';
+import image3 from '../../assets/target3.png';
 
-function Popupmenu({ position }) {
+function Popupmenu({ position, checkTarget }) {
   const posX = position[0];
   const posY = position[1];
 
@@ -15,9 +20,33 @@ function Popupmenu({ position }) {
         left: `${posX + 25}px `
       }}>
       <ul>
-        <li>Char1</li>
-        <li>Char2</li>
-        <li>Char3</li>
+        <li>
+          <img
+            src={image1}
+            alt="target1"
+            onClick={() => {
+              checkTarget('target1');
+            }}
+          />
+        </li>
+        <li>
+          <img
+            src={image2}
+            alt="target2"
+            onClick={() => {
+              checkTarget('target2');
+            }}
+          />
+        </li>
+        <li>
+          <img
+            src={image3}
+            alt="target3"
+            onClick={() => {
+              checkTarget('target3');
+            }}
+          />
+        </li>
       </ul>
     </div>
   );
@@ -26,5 +55,6 @@ function Popupmenu({ position }) {
 export default Popupmenu;
 
 Popupmenu.propTypes = {
-  position: PropTypes.arrayOf(PropTypes.number).isRequired
+  position: PropTypes.arrayOf(PropTypes.number).isRequired,
+  checkTarget: PropTypes.func.isRequired
 };
