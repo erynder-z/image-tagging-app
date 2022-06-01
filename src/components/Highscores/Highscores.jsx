2c87723b97c82dfcc5d5c4fc06b290c151d9badf
erynder-z <stfn.bgr@gmail.com>
2022-06-01 13:21:36 +0200

    Display current highscores

---
+++ src/components/Highscores/Highscores.jsx
@@ -0,0 +1,29 @@
+import './Highscores.css';
+import React from 'react';
+import { query, collection, orderBy, limit } from 'firebase/firestore';
+import { useCollectionData } from 'react-firebase-hooks/firestore';
+import database from '../Firebase/Firebase';
+
+function Highscores() {
+  const highscoresRef = collection(database, 'highscores');
+  const q = query(highscoresRef, orderBy('time'), limit(10));
+  const [highscores] = useCollectionData(q, { idField: 'id' });
+
+  return (
+    <div className="highscores-container">
+      <div className="highscore-container">
+        {highscores &&
+          highscores.map((highscore) => (
+            <div key={highscore.id.toString()} className="highscore-item">
+              <div className="highscore-item-name">{highscore.name}</div>
+              <div className="highscore-item-time">
+                {highscore.time.minutes}:{highscore.time.seconds}
+              </div>
+            </div>
+          ))}
+      </div>
+    </div>
+  );
+}
+
+export default Highscores;
