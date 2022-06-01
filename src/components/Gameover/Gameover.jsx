47b395e8847ffdda24ba17c9f20b056401e92a75
erynder-z <stfn.bgr@gmail.com>
2022-06-01 19:03:52 +0200

    check if user score is a highscore

--- src/components/Gameover/Gameover.jsx
+++ src/components/Gameover/Gameover.jsx
@@ -3,10 +3,14 @@ import PropTypes from 'prop-types';
 import React, { useEffect, useState } from 'react';
 import { intervalToDuration } from 'date-fns';
 import { Link } from 'react-router-dom';
+import { query, collection, getDocs, orderBy, limit } from 'firebase/firestore';
+import Highscores from '../Highscores/Highscores';
+import database from '../Firebase/Firebase';
 
 function Gameover({ user, resetGame }) {
   const [formValue, setFormValue] = useState('');
   const [userGameover, setUserGameover] = useState(user);
+  const [isHighscore, setIsHighscore] = useState(false);
 
   const updateUser = (e) => {
     e.preventDefault();
@@ -18,7 +22,7 @@ function Gameover({ user, resetGame }) {
     }
   };
 
-  useEffect(() => {
+  const updateUserObject = () => {
     const unixTimeEnd = Date.now();
     // returns date-fns Duration-object: https://date-fns.org/v2.28.0/docs/Duration
     const difference = intervalToDuration({ start: userGameover.gameStart, end: unixTimeEnd });
@@ -27,7 +31,38 @@ function Gameover({ user, resetGame }) {
       gameFinish: unixTimeEnd,
       time: difference
     }));
+  };
+
+  const isUserscoreHighscore = async () => {
+    const checkHighscores = (userScoreObject, onlineScoresArray) => {
+      const slower = (onlineScoreObject) =>
+        onlineScoreObject.gameFinish - onlineScoreObject.gameStart >
+        userScoreObject.gameFinish - userScoreObject.gameStart;
+      if (onlineScoresArray.some(slower)) {
+        setIsHighscore(true);
+      }
+    };
+    const getHighscores = async () => {
+      const highscoresRef = collection(database, 'highscores');
+      const q = query(highscoresRef, orderBy('time'), limit(10));
+      const querySnapshot = await getDocs(q);
+      return querySnapshot.docs.map((doc) => doc.data());
+    };
+
+    const checked = await getHighscores();
+
+    return checkHighscores(userGameover, checked);
+  };
+
+  useEffect(() => {
+    updateUserObject();
   }, []);
+
+  useEffect(() => {
+    if (userGameover.gameFinish !== '') {
+      isUserscoreHighscore();
+    }
+  }, [userGameover.gameFinish]);
 
   const formattedMinutes = userGameover.time.minutes.toLocaleString('en-US', {
     minimumIntegerDigits: 2,
@@ -43,32 +78,41 @@ function Gameover({ user, resetGame }) {
     <div className="gameover-overlay">
       <div className="gameover-body">
         <h3>Something</h3>
+        <Link
+          to="/"
+          onClick={() => {
+            resetGame();
+          }}>
+          Return to Main
+        </Link>
         <h3>
           your time: {formattedMinutes}:{formattedSeconds}
         </h3>
-        <form
-          action="input"
-          onSubmit={(e) => {
-            updateUser(e);
-          }}>
-          <input
-            type="text"
-            placeholder="enter your name"
-            value={formValue}
-            onChange={(e) => {
-              setFormValue(e.target.value);
-            }}
-          />
+        {isHighscore === true && (
+          <>
+            <h3>Congratulations! You got a highscore!</h3>
+            <div className="highscore-input-container">
+              <form
+                action="input"
+                onSubmit={(e) => {
+                  updateUser(e);
+                }}>
+                <input
+                  type="text"
+                  placeholder="enter your name"
+                  value={formValue}
+                  onChange={(e) => {
+                    setFormValue(e.target.value);
+                  }}
+                />
 
-          <button type="submit">Submit</button>
-          <Link
-            to="/"
-            onClick={() => {
-              resetGame();
-            }}>
-            Return to Main
-          </Link>
-        </form>
+                <button type="submit">Submit</button>
+              </form>
+            </div>
+          </>
+        )}
+        {isHighscore === false && <h3>Sorry, no highscore!</h3>}
+        <Highscores />
       </div>
     </div>
   );
